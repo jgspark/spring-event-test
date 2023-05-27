33ace18453ac0bbe6afe92732169ca5e8b08d9c5
spark <pnw123456@icloud.com>
2023-05-27 23:27:33 +0900

    build: h2 database conntion add

--- src/main/java/com/example/springeventtest/domain/Order.java
+++ src/main/java/com/example/springeventtest/domain/Order.java
@@ -1,4 +1,19 @@
 package com.example.springeventtest.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
 }
