6a7700070868f9f575f40627c7ad6864c34324a1
spark <pnw123456@icloud.com>
2023-05-29 19:23:00 +0900

    style: 코드 정리

--- src/main/java/com/example/springeventtest/domain/event/AsyncEventListener.java
+++ src/main/java/com/example/springeventtest/domain/event/AsyncEventListener.java
@@ -6,8 +6,6 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.util.Optional;
 
